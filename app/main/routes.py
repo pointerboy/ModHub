241adafc7e0ceee60108dcbc6e1ee236558b45f9
pointerboy <lukatrbovic@outlook.com>
2020-05-20 18:58:48 +0200

    fixed picture not saving

--- app/main/routes.py
+++ app/main/routes.py
@@ -137,7 +137,7 @@ def save_picture(form_picture):
     secur_filename = secure_filename(form_picture.filename)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(os.path.join(current_app.root_path, 'static/profile_pics', picture_fn))
 
     return picture_fn
 
