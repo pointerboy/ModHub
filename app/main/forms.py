8ccb10f4ab95335fc3c00388806818fb8fe1e7e9
pointerboy <lukatrbovic@outlook.com>
2020-05-26 15:13:42 +0200

    fixed max ttpo

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,7 +11,7 @@ from app.models import User
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
     about_me = TextAreaField(_l('About me'),
-                             validators=[Length(min=10, 250)])
+                             validators=[Length(min=10, max=250)])
 
     profile_pic = FileField(_l('New profile picture'), validators=[FileAllowed(['jpg', 'png'], 'Invalid file set for the profile picture. We support jpg and png.')])
     submit = SubmitField(_l('Submit'))
