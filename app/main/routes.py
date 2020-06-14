c3cd5b72a568428fd6463684ffe56b0201a80919
pointerboy <lukatrbovic@outlook.com>
2020-06-14 22:17:13 +0200

    fixed download crash

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,6 +13,10 @@ from app.main import bp
 from markdown import markdown
 
 import functools
+import os	
+from os import urandom	
+import binascii
+
 def has_role(name):
     def real_decorator(f):
         def wraps(*args, **kwargs):
