1b2a51c36288084564f8fd3f8d9ab3d493cc81e7
H2O <ahmedh2o1999@gmail.com>
2020-06-19 11:35:12 +0200

    Don't show download button without a valid download path

--- microblog.py
+++ microblog.py
@@ -1,7 +1,8 @@
-from app import create_app, db, cli
+from app import create_app, db, cli, current_app
 from app.models import User, Post, Message, Notification, Task
 
 from flask_mobility import Mobility
+import os
 
 app = create_app()
 cli.register(app)
@@ -12,3 +13,10 @@ Mobility(app)
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
             'Notification': Notification, 'Task': Task}
+
+def is_valid_download(filename):
+    file_loc = os.path.join(current_app.root_path, 'static/moduploads', filename)
+    if os.path.isfile(file_loc):
+        return True
+    else: return False
+app.jinja_env.globals.update(is_valid_download=is_valid_download)
